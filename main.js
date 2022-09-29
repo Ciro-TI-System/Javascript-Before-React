30b8387513a9d68e5651428b9e2a98f29819001b
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2022-09-29 11:13:12 -0300

    Imports and exports

---
+++ main.js
@@ -0,0 +1,10 @@
+import { subtract, PI, dividir as divide } from "./lib/math"
+import * as math from "./lib/math"
+import { add } from "./lib/sum";
+
+console.log(add(1, 2))
+console.log(subtract(1, 2))
+console.log(PI)
+console.log(math)
+console.log(math.subtract(255, 157))
+console.log(divide(125, 5))
